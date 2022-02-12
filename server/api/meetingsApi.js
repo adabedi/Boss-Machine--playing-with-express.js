acefdd2306ec0e9c14c512fd7ed2b7ac72314c91
adabedi <adrianna.bedychaj@gmail.com>
2022-02-12 15:13:35 +0000

    Mettings api- updated to delete all mittings

--- server/api/meetingsApi.js
+++ server/api/meetingsApi.js
@@ -5,13 +5,11 @@ const meetingsSchema = require('../schema/meetingSchema');
 const {
   getAllFromDatabase,
   addToDatabase,
-  deleteFromDatabasebyId,
+  deleteAllFromDatabase,
 } = require('../db');
 const { validate } = require('../ErrorHandling');
-const { validateID } = require('./middlewareUtils');
 
 const DB_MODEL = 'meetings';
-meetingsRouter.param('id', validateID(DB_MODEL));
 
 meetingsRouter.get('/', (_, res) => {
   res.send(getAllFromDatabase(DB_MODEL));
@@ -19,11 +17,11 @@ meetingsRouter.get('/', (_, res) => {
 
 meetingsRouter.post('/', validate({ body: meetingsSchema }), (req, res) => {
   const newMeeting = addToDatabase(DB_MODEL, req.body);
-  res.status(200).send(newMeeting);
+  res.status(201).send(newMeeting);
 });
 
-meetingsRouter.delete('/:id', (req, res) => {
-  deleteFromDatabasebyId(DB_MODEL, req.id);
+meetingsRouter.delete('/', (_, res) => {
+  deleteAllFromDatabase(DB_MODEL);
   res.status(204).send();
 });
 
